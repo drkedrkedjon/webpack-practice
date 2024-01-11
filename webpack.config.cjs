534500f3019cbaf63a06ad43b69f405fd5966c3b
Sasa Savic Perusina <sasa@bubulazy.com>
2024-01-11 13:30:26 +0000

    Added comment

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,3 +1,4 @@
+// Path is to have acces to files and folders
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
