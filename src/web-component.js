ab95c305429e76a38237cce4e9aa2b94d6e92f4a
Sasa Savic Perusina <sasa@bubulazy.com>
2024-01-12 11:38:27 +0000

    Made Lit web-Component to substitute h1 tag

---
+++ src/web-component.js
@@ -0,0 +1,25 @@
+import { css, html, LitElement } from "lit";
+
+export class WebComponentTitle extends LitElement {
+  constructor() {
+    super();
+  }
+
+  static styles = css`
+    h1 {
+      font-size: 3.5rem;
+      font-weight: 900;
+      padding: 1.5rem 0;
+      margin: 0;
+      color: var(--primary-color);
+    }
+  `;
+
+  render() {
+    return html`<h1>
+      Guía de estilos de <br />
+      The Vegan Restaurant
+    </h1>`;
+  }
+}
+window.customElements.define("web-component-title", WebComponentTitle);
